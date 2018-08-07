1680ef61bca3829e48a1a9288ce817e199cfe935
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 14:12:27 +0300

    Flask intro part 3 FINISHED

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    pass
+	fav_players = ["Messi","Neymar","someone"],
+    return render_template("index.html"),
+    fav_players=fav_players
 
 if __name__ == '__main__':
    app.run(debug = True)
